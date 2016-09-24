3ac29415a24965b09d672ebd7440a9cbafcb1cb2
Otto Giron <ottog2486@gmail.com>
2016-09-24 13:26:41 -0600

    #9 Update to only run elastic test in travis by default

--- backend/elastic/elastic_test.go
+++ backend/elastic/elastic_test.go
@@ -1,12 +1,16 @@
 package elastic
 
 import (
+	"os"
 	"testing"
 
 	"github.com/ottogiron/ferrariworker/backend"
 )
 
 func TestBackend(t *testing.T) {
+	if os.Getenv("ELASTIC_BACKEND_TEST") != "true" {
+		t.Skip("Skiping elastic test")
+	}
 	b := New()
 	backend.TestBackend(t, b)
 }
