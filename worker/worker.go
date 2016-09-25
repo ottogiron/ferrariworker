8e70abe911e32982b6461d0274a3b288e7a989d0
Otto Giron <ottog2486@gmail.com>
2016-09-24 19:10:07 -0600

    #9 BAsic implementation of persist method

--- worker/worker.go
+++ worker/worker.go
@@ -10,12 +10,12 @@ type Message struct {
 
 //JobResult Represents the result of a processed Job
 type JobResult struct {
-	ID        string
-	WorkerID  string
-	Status    JobStatus
-	Output    []byte
-	StartTime time.Time
-	EndTime   time.Time
+	ID        string    `json:"job_id"`
+	WorkerID  string    `json:"worker_id"`
+	Status    JobStatus `json:"status"`
+	Output    []byte    `json:"output"`
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
 }
 
 type JobStatus int
